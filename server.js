da12eec2bcee3e5cc18cf8bb9d7147a27d7fde0f
abduvalinazirov <abduvali.nazirov.0@gmail.com>
2022-03-08 17:51:12 +0500

    ready to deploy

--- server.js
+++ server.js
@@ -5,15 +5,19 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 
-app.use('/api', require('./routes/StudentRoutes'))
+app.use("/api", require("./routes/StudentRoutes"));
 
-const PORT = process.env.PORT;
-const MongoUrl = process.env.MONGO_URL;
+// const PORT = process.env.PORT;
+// const MongoUrl = process.env.MONGO_URL;
 
-mongoose.connect(MongoUrl, { useNewUrlParser: true }, () => {
-  console.log("db connected");
-});
+mongoose.connect(
+  "mongodb+srv://user:admin@cluster0.yrh0r.mongodb.net/curd?retryWrites=true&w=majority",
+  { useNewUrlParser: true },
+  () => {
+    console.log("db connected");
+  }
+);
 
-app.listen(PORT, () => {
+app.listen(5009, () => {
   console.log("server is running");
 });
